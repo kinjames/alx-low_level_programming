582491b9aef8076864e8cec0fedf22f1a5537ef9
James Kofi Myers <myers_james59@yahoo.com>
2022-10-14 13:58:37 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char lowerCase, upperCase;
+for (lowerCase = a; lowerCase <= z; lowerCase++)
+putchar(lowerCase);
+for (upperCase = A; upperCase <= Z; upperCase++)
+putchar(upperCase);
+putchar(n);
+return (0);
+}
