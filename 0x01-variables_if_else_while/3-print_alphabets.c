d522d5512ee9dd19c53e1392a7ab9adfe7007e1d
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2022-10-14 21:12:07 +0000

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,11 +6,13 @@
  */
 int main(void)
 {
-char lowerCase, upperCase;
-for (lowerCase = a; lowerCase <= z; lowerCase++)
-putchar(lowerCase);
-for (upperCase = A; upperCase <= Z; upperCase++)
-putchar(upperCase);
-putchar(n);
-return (0);
+	char lowerCase, upperCase;
+
+	for (lowerCase = 'a'; lowerCase <= 'z'; lowerCase++)
+		putchar(lowerCase);
+	for (upperCase = 'A'; upperCase <= 'Z'; upperCase++)
+		putchar(upperCase);
+	putchar('\n');
+
+	return (0);
 }
