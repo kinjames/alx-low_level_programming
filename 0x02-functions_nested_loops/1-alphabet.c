a8f0f205372dcc7d805dbe7abed97c39533f5573
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2022-10-18 21:44:42 +0000

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,14 @@
 #include "main.h"
+
 /**
-*print_alphabet-prints all alphabets in lowercase
-*Return: On success 1.
-*/
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ */
 void print_alphabet(void)
 {
-char i;
-for(i-'a';i<='z';i++)
-{
-_putchar(i);
-}
-_putchar('\n');
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
 }
