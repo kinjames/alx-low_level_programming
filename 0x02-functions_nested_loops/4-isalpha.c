fd04c0db885c30d217789ce581b6db9f4dc74e19
James Kofi Myers <myers_james59@yahoo.com>
2022-10-18 15:39:44 -0700

    Task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,12 +6,11 @@
  *
  * Return: 1 if character is letter, lowercase or uppercase, 0 otherwise.
  */
-int _isalpha(int c)
-  
+int _isalpha(int c)  
 {
 if ((c >= 'a' && c <= 'z') ||
-(c >= 'A' && c <= 'Z'))   
-return (1); 
+(c >= 'A' && c <= 'Z'))
+return (1);
 else
 return (0); 
 }
