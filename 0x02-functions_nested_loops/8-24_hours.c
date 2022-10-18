9fffc8dc54d1fd8554888a5b030614cd3ece34a3
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2022-10-18 23:55:06 +0000

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,8 +6,8 @@
 
 void jack_bauer(void)
 {
-int hour, minute;  
-for (hour = 0; hour <= 23; hour++)    
+int hour, minute;
+for (hour = 0; hour <= 23; hour++)
 {
 for (minute = 0; minute <= 59; minute++)
 {
@@ -16,7 +16,7 @@ _putchar((hour % 10) + '0');
 _putchar(':');
 _putchar((minute / 10) + '0');
 _putchar((minute % 10) + '0');
-_putchar('\n');	  
-}      
-} 
+_putchar('\n');
+}
+}
 }
