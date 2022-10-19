6604cef248a96d057a01856ecf9aa5621820bf2b
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2022-10-19 00:46:22 +0000

    Update 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,7 +6,7 @@
 * Return: Always 0.
 */
 int main(void)
-{  
+{
 int count;
 unsigned long fib1 = 0, fib2 = 1, sum;
 for (count = 0; count < 50; count++)
@@ -14,11 +14,11 @@ for (count = 0; count < 50; count++)
 sum = fib1 + fib2;
 printf("%lu", sum);
 fib1 = fib2;
-fib2 = sum;      
+fib2 = sum;
 if (count == 49)
-printf("\n");  
+printf("\n");
 else
 printf(", ");
 }
-return (0); 
+return (0);
 }
