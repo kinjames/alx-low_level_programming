69bac37ff8967bd89281e47b5cf7618979f40e88
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2022-10-20 22:57:12 +0000

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,37 +1,18 @@
 #include "main.h"
 
-
-
 /**
-
- *print_most_numbers - prints
-
- * owned by Bwave ICT
-
- *Return: void
-
- */
-
-
+*print_most_numbers - prints
+*Return: void
+*/
 
 void print_most_numbers(void)
-  
 {
-  
-  char c;
-  
+	char c;
 
-  
-  for (c = '0'; c <= '9'; c++)
-    
-    {
-      
-      if (!(c == '2' || c == '4'))
-	
+	for (c = '0'; c <= '9'; c++)
+	{
+	if (!(c == '2' || c == '4'))
 	_putchar(c);
-      
-    }
-  
-  _putchar('\n');
-  
+	}
+	_putchar('\n');
 }
