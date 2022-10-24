c2db2a745d12e66ef6b29c8423926cd57b22f68d
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2022-10-24 21:50:36 +0000

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * puts2 - prints every other character of a string
  *
@@ -7,21 +6,12 @@
  *
  * Return: 0 is success
  */
-
 void puts2(char *str)
-  
 {
-  
-  int string;
-  
+	int string;
 
-  
-  for (string = 0; str[string] != '\0'; string++)
-    
-    if (string % 2 == 0)
-      
-      _putchar(str[string]);
-  
-  _putchar('\n');
-  
+	for (string = 0; str[string] != '\0'; string++)
+	if (string % 2 == 0)
+		_putchar(str[string]);
+	_putchar('\n');
 }
