77dec0c697f5c0e63c97501b7657e06b20e56100
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2022-10-27 03:38:11 +0000

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -6,41 +6,22 @@
  *
  * Return: address of s
  */
-
 char *rot13(char *s)
-  
 {
-  
-  int i, j;
-  
-  char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  
-  char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
-  
+	int i, j;
+	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-  
-  for (i = 0; *(s + i); i++)
-    
-    {
-      
-      for (j = 0; j < 52; j++)
-	
+	for (i = 0; *(s + i); i++)
 	{
-	  
-	  if (a[j] == *(s + i))
-	    
-	    {
-	      
-	      *(s + i) = b[j];
-	      
-	      break;
-	      
-	    }
-	  
+		for (j = 0; j < 52; j++)
+		{
+			if (a[j] == *(s + i))
+			{
+				*(s + i) = b[j];
+				break;
+			}
+		}
 	}
-      
-    }
-  
-  return (s);
-  
+	return (s);
 }
