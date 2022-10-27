146e69e282a74e3ec16353e6f5a8d42c55ccfe78
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2022-10-27 03:27:19 +0000

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,33 +9,18 @@
  *
  * Return: pointer to the resulting string dest
  */
-
 char *_strncat(char *dest, char *src, int n)
-  
 {
-  
-  int i, j;
-  
+	int i, j;
 
-  
-  for (i = 0; dest[i] != '\0'; i++)
-    
-    {
-      
-      continue;
-      
-    }
-  
-  for (j = 0; src[j] != '\0' && j < n; j++)
-    
-    {
-      
-      dest[i + j] = src[j];
-      
-    }
-  
-  dest[i + j] = '\0';
-  
-  return (dest);
-  
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		continue;
+	}
+	for (j = 0; src[j] != '\0' && j < n; j++)
+	{
+		dest[i + j] = src[j];
+	}
+	dest[i + j] = '\0';
+	return (dest);
 }
