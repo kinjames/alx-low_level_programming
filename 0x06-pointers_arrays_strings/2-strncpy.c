17719d6b75b34140460f9c45e5a7c51a90dc8b36
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2022-10-27 03:29:17 +0000

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,33 +9,18 @@
  *
  * Return: pointer to the resulting string dest
  */
-
 char *_strncpy(char *dest, char *src, int n)
-  
 {
-  
-  int i;
-  
+int i;
 
-  
-  for (i = 0; i < n && *(src + i); i++)
-    
-    {
-      
-      *(dest + i) = *(src + i);
-      
-    }
-  
-  for (; i < n; i++)
-    
-    {
-      
-      *(dest + i) = '\0';
-      
-    }
-  
-  return (dest);
-  
+	for (i = 0; i < n && *(src + i); i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+	for (; i < n; i++)
+	{
+	*(dest + i) = '\0';
+	}
+	return (dest);
 
-  
 }
