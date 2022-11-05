ebbb7ea7bed7c965a92a4f4b2c0b044013d83772
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2022-11-05 00:59:33 +0000

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- * main - Entry point
+ * main - prints its name, followed by a new line.
  * @argc: argument count
- * @argv: argument vector
- * Return: success
+ * @argv: arguments
+ *
+ * Return: 0
  */
-
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-  (void)argc;
+	(void)argc;
 
-  printf("%s\n", argv[0]);
-
-  exit(EXIT_SUCCESS);
+	printf("%s\n", argv[0]);
+	return (0);
 }
