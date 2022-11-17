b3f2e2fe025adde66a4d0976629fce683a8f8535
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2022-11-17 19:50:20 +0000

    Update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,19 +8,19 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-  int sum;
-  unsigned int i;
-  va_list ap;
+	int sum;
+	unsigned int i;
+	va_list ap;
 
-  va_start(ap, n);
+	va_start(ap, n);
 
-  if (n == 0)
-    return (0);
+	if (n == 0)
+		return (0);
 
-  for (i = 0; i < n; i++)
-    sum += va_arg(ap, int);
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, int);
 
-  va_end(ap);
+	va_end(ap);
 
-  return (sum);
+	return (sum);
 }
