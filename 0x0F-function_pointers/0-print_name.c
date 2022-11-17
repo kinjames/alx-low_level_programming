a1cb584b0cdbe966c745703464dea1fd8847415c
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2022-11-17 01:54:42 +0000

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -7,15 +7,9 @@
  *
  * Rteurn: void
  */
-
 void print_name(char *name, void (*f)(char *))
-  
 {
-  
-  if (name != NULL && f != NULL)
-    
+	if (name != NULL && f != NULL)
 
-    
-    f(name);
-  
+		f(name);
 }
