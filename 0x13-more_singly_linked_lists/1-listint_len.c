553d586af32a25678dfcdca2b2f29bda7a5f9be3
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2022-11-28 19:48:04 +0000

    Update 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,43 +1,22 @@
 #include "lists.h"
 
-
-
 /**
-
  * listint_len - the number of elements in a linked listint_t list.
-
  *
-
  * @h: head pointer
-
  *
-
  * Return: the number of elements
-
  *
-
  */
 
-
-
 size_t listint_len(const listint_t *h)
-  
 {
-  
-  size_t count = 0;
-  
+	size_t count = 0;
 
-  
-  while (h != NULL)
-    
-    {
-      
-      h = h->next;
-      
-      count++;
-      
-    }
-  
-  return (count);
-  
+	while (h != NULL)
+	{
+	h = h->next;
+		count++;
+	}
+	return (count);
 }
