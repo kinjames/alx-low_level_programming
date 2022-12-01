cd598a1d4140da94bb64d825613ddabc99c7698c
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2022-12-01 23:23:50 +0000

    Update 100-get_endianness.c

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,31 +1,16 @@
 #include "main.h"
-
 /**
-
  * get_endianness - checks the endianness
-
  *
-
  * Return: 0 if big endian, 1 if little endian
-
  */
-
 int get_endianness(void)
-  
 {
-  
-  int i;
-  
-  char *c;
-  
+	int i;
+	char *c;
 
-  
-  i = 1;
-  
-  c = (char *)&i;
-  
+	i = 1;
+	c = (char *)&i;
 
-  
-  return (*c);
-  
+	return (*c);
 }
